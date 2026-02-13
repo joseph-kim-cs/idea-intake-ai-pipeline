569de578ea954dc714005b334cbbeaa00ee0ce0d
Joseph Kim <josephkim@Josephs-MBP.lan>
2026-02-13 04:54:17 -0800

    setup for pipeline analysis

--- scripts/pipeline_analysis.py
+++ scripts/pipeline_analysis.py
@@ -1,5 +1,28 @@
 import pandas as pd
 from ibm_watsonx_ai.foundation_models import ModelInference
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+PROJECT_ID = os.getenv('PROJECT_ID')
+MODEL_ID = os.getenv('MODEL_ID')
+URL = os.getenv('URL')
+
+PARAMS = {
+    "decoding_method": "greedy",
+    "max_new_tokens": 300,
+    "min_new_tokens": 0,
+    "repetition_penalty": 1
+}
+
+model = ModelInference(
+    model_id=MODEL_ID,
+    params=PARAMS,
+    credentials={
+        "url": URL,
+        "apikey": API_KEY
+    },
+    project_id=PROJECT_ID,
+)
